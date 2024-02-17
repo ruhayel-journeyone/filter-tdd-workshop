bd6297a74ee257ebf200f6306e1d320cbf4a4bd6
Rami Ruhayel <rami@ruhayel.com>
2024-02-17 09:00:39 +0800

    GREEN: Given a callback function and an array, it returns an array

--- src/filter.js
+++ src/filter.js
@@ -2,5 +2,6 @@ function filter(callbackFunction, array) {
   for (let index = 0; index < array.length; index++) {
     callbackFunction(array[index]);
   }
+  return array;
 }
 export default filter;
